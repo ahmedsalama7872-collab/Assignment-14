9831066d2c57af4de9a5a95b8f26a520602f5f5f
ahmed <ahmedsalama7872@gmail.com>
2026-09-15 10:39:52 +0300

    refactor: Switch from createHashRouter to createBrowserRouter for routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import Home from './Pages/Home.jsx'
 import Post from './Pages/Post.jsx'
-import Layout from './components/Layout.jsx' // لو كنت تستخدم Layout رئيسي
-import { createBrowserRouter, RouterProvider,createHashRouter } from 'react-router-dom' // <- التعديل هنا (إضافة -dom)
+import Layout from './components/Layout.jsx' 
+import { createBrowserRouter, RouterProvider } from 'react-router-dom' 
 import Latest from './components/Latest.jsx'
 import Blog from './Pages/Blog.jsx'
 
-// تعريف المسارات
-const routes = createHashRouter([
+
+// تجربه
+
+
+const routes = createBrowserRouter([
   {path:'', element:<Layout/>, children:[
     {index:true,element:<Home/>},
     {path:"home" ,element:<Home/>},
