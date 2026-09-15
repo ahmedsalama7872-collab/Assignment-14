465b167af5c48a0cb21a8d18f4c4b044e479815e
ahmed <ahmedsalama7872@gmail.com>
2026-09-15 10:26:09 +0300

    fix: Update routing to use createHashRouter for improved navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
-import React from 'react'
 import Home from './Pages/Home.jsx'
 import Post from './Pages/Post.jsx'
-import Subscribe from './components/Subscribe.jsx'
 import Layout from './components/Layout.jsx' // لو كنت تستخدم Layout رئيسي
-import { createBrowserRouter, RouterProvider } from 'react-router-dom' // <- التعديل هنا (إضافة -dom)
+import { createBrowserRouter, RouterProvider,createHashRouter } from 'react-router-dom' // <- التعديل هنا (إضافة -dom)
 import Latest from './components/Latest.jsx'
 import Blog from './Pages/Blog.jsx'
 
 // تعريف المسارات
-const routes = createBrowserRouter([
+const routes = createHashRouter([
   {path:'', element:<Layout/>, children:[
     {index:true,element:<Home/>},
     {path:"home" ,element:<Home/>},
